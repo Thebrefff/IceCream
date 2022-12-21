f93ce11c9e2684bf4c6ff497cfbcd1ba598dde5f
Filip Vukić <filipvukic90@gmail.com>
2022-12-21 19:57:22 +0100

    added scss for mobile-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,11 +1,11 @@
 const menuOverlay = document.querySelector('.mobile-menu');
 const closeMenuButton = document.querySelector('.toggle-menu');
-const openMenuBtn = document.querySelector('.header__button-menu');
+const openMenuBtn = document.querySelector('.header__button-icon');
 
 openMenuBtn.addEventListener('click', e => {
-  menuOverlay.classList.remove('d-none');
+  menuOverlay.classList.remove('display-none');
 });
 
 closeMenuButton.addEventListener('click', e => {
-  menuOverlay.classList.add('d-none');
+  menuOverlay.classList.add('display-none');
 });
